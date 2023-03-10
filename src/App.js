1a350505a8e9678f49390ae04e821c29de37bda2
Rodriguejean <rodjean.tech@gmail.com>
2023-03-10 14:44:52 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+// import logo from './logo.svg';
+import './App.css';
+import Article from './components/Article';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Nav from './components/Nav';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Nav />
+      <Article />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
